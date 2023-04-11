ff5decb09fcdb6fb2c37cc90d70ffa99cd9a2d98
AndreNovaisBrito <andrenoz@gmail.com>
2023-04-11 07:26:42 -0300

    first commit

---
+++ src/modules/products/typeorm/Product.ts
@@ -0,0 +1,28 @@
+import {
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  Column,
+} from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
