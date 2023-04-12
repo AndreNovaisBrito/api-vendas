1b2d1484f85874a40d98f8c628547914cae74efd
AndreNovaisBrito <andrenoz@gmail.com>
2023-04-12 08:49:53 -0300

    validation finished

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -12,10 +12,10 @@ class CreateProductService {
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
     const productsRepository = getCustomRepository(ProductRepository);
     const productExists = await productsRepository.findByName(name);
-
     if (productExists) {
       throw new AppError('There is already one product with this name');
     }
+
     const product = productsRepository.create({
       name,
       price,
