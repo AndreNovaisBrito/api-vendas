d951375ac03b48e40f20f85225a7ce826ad55dd3
AndreNovaisBrito <andrenoz@gmail.com>
2023-04-13 08:44:29 -0300

    token ok

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,14 +3,20 @@ import AppError from '@shared/errors/AppError';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
     if (!user) {
@@ -23,7 +29,14 @@ class CreateSessionsService {
       throw new AppError('Incorrect email/password combination 2', 401);
     }
 
-    return user;
+    const token = sign({}, '726c5bce5aec4f2453a29af09fbb2f44', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+    return {
+      user,
+      token,
+    };
   }
 }
 
