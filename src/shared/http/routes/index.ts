584fdca74926cde7f34c33492cab6782f99aa2ef
AndreNovaisBrito <andrenoz@gmail.com>
2023-04-17 11:03:06 -0300

    customers done

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import customersRouter from '@modules/customers/routes/customers.routes';
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
@@ -11,4 +12,5 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
 export default routes;
