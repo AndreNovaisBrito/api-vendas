4b94738e9e2c9dc6a587918dfa8e3f813590c569
AndreNovaisBrito <andrenoz@gmail.com>
2023-04-19 11:42:28 -0300

    cache done =D

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 import AppError from '@shared/errors/AppError';
+import RedisCache from '@shared/cache/RedisCache';
 
 interface IRequest {
   name: string;
@@ -21,6 +22,10 @@ class CreateProductService {
       price,
       quantity,
     });
+
+    const redisCache = new RedisCache();
+
+    await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     await productsRepository.save(product);
 
